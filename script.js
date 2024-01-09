4dcd351543ca9d3b1d095628d617199c9996608e
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-01-09 11:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let count = 0;
+document.getElementById('counter').InnerHtml = count;
+function Increment(){
+	alert(count);
+	count+=1;
+	document.getElementById('counter').textContent = count;
+}
